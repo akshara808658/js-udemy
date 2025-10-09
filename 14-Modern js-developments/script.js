29f53240a0930bdb70c46c05293c151074b823c5
Akshara <pakshara.2005@gmail.com>
2025-10-09 21:24:46 +0530

    JS modules and npm packages

--- 14-Modern js-developments/script.js
+++ 14-Modern js-developments/script.js
@@ -1,15 +1,39 @@
 //Importing module
 // import {addToCart,totalPrice as price ,tq} from './shoppingCart.js';
 
-console.log('Importing module')
-import * as ShoppingCart from './shoppingCart.js'
+// console.log('Importing module')
+// import * as ShoppingCart from './shoppingCart.js'
 
-ShoppingCart.addToCart('bread', 7)
+// ShoppingCart.addToCart('bread', 7)
 
-console.log(ShoppingCart.totalPrice)
+// console.log(ShoppingCart.totalPrice);
+
+//by default--
+import add, { cart } from './shoppingCart.js';
+add('Pizza', 2);
+add('bread', 5);
+add('apples',4);
+console.log(cart);
+
 // addToCart('bread', 5);
 // console.log(price,tq)
 //
+async function getLastPost(){
+   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+   const data = await res.json();
+   console.log(data);
+
+   return {title: data.at(-1).title, text: data.at(-1).body};
+};
+const lastPost = getLastPost();
+console.log(lastPost);
+
+//Not very clean
+// lastPost.then(last => console.log(last));
+
+//
+
+//-----------------------------------------------------------------------------------------------------------------
 //Modern Js Development 
 //Module
 /**
@@ -24,3 +48,44 @@ console.log(ShoppingCart.totalPrice)
          IIFE(immediate invoked fn expression) (function(){}()): wrap variable in local scope
          Modules: use export/import 
       */
+//
+//--------
+//module pattern
+const ShoppingCart2 = (function() {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity){
+   cart.push({product, quantity});
+   console.log(`${quantity} ${product} added to cart`);
+  };
+  return {
+   addToCart,
+   cart,
+   totalPrice,
+   totalQuantity,
+  }
+})();
+ShoppingCart2.addToCart('apple' , 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
+/*
+//common js modules
+//Export 
+exports.addToCart = function (product,quantity){
+   cart.push({product,quantity});
+   console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+   );
+};
+
+//Import
+const { addToCart} = require('./shoppingCart.js');
+ */
+
+//package.json : as the heart of a Node.js . it stores the metadate and configuration about your project
+  // without this npm would not know  which packages your needs (run --> npm start)
+//package-lock.json : locks exact versions of dependencies for repoducibility
--- 14-Modern js-developments/shoppingCart.js
+++ 14-Modern js-developments/shoppingCart.js
@@ -1,16 +1,35 @@
 //Exporting module
 console.log('Exporting Module');
 
+//Blocking code
+console.log('Finish fetching users');
+await fetch('https://jsonplaceholder.typicode.com/users');
+console.log('Finish fetching users')
+
+
 const shippingCost = 10;
-const cart = [];
+export const cart = [];
 
 export function addToCart(product, quantity){
   cart.push({product,quantity});
-  console.log(`${quantity} ${product} added to cart`)
+  console.log(`${quantity} ${product} added to cart`) 
 }
 
 const totalPrice = 234;
 const totalQuantity = 23;
 
-export{totalPrice,totalQuantity as tq}
+export{totalPrice,totalQuantity as tq};
 
+export default function(product,quantity){
+  cart.push({product, quantity});
+  console.log(`${quantity}${product} added to cart`);
+}
+
+/**
+ * Export : export const cart = [] , make variable or fn available to others
+ * Import : import { cart } from './file.js' , use exported code in another file 
+ * Default Export : export dafault fn() , only one per file; import without {}
+ * top-level await : await fetch() , works only in modules
+ * module pattern : (function (){..}()) , old way to create private/public code
+ * avoid global scope: use modules or IIFE , prevents conflicts
+ */
